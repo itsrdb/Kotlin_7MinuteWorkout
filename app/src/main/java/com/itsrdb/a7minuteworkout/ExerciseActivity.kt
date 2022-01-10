50e27b00e8b1de9ce809c190d0c46fd6527c8cf0
Rohit D Borkar <rohitborkar00@gmail.com>
2022-01-10 18:56:49 +0530

    Added exercise progress bar

--- app/src/main/java/com/itsrdb/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/itsrdb/a7minuteworkout/ExerciseActivity.kt
@@ -3,6 +3,8 @@ package com.itsrdb.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
@@ -13,6 +15,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0    //progress left
 
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0    //progress left
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +48,29 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "We will start the exercise now", Toast.LENGTH_SHORT).show()
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        val llRest = findViewById<LinearLayout>(R.id.llRestView)
+        val llExercise = findViewById<LinearLayout>(R.id.llExerciseView)
+        llRest.visibility = View.GONE
+        llExercise.visibility = View.VISIBLE
+        val progressBarEx = findViewById<ProgressBar>(R.id.progressBarExercise)
+        val tvTimerEx = findViewById<TextView>(R.id.tvExerciseTimer)
+        progressBarEx.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000){
+            override fun onTick(p0: Long) { //Every single tick
+                exerciseProgress++
+                progressBarEx.progress = 30-exerciseProgress
+                tvTimerEx.text = (30-exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Moving ahead", Toast.LENGTH_SHORT).show()
             }
         }.start()
     }
@@ -62,7 +88,14 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer!!.cancel()
             restProgress = 0
         }
-
         setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
     }
 }
