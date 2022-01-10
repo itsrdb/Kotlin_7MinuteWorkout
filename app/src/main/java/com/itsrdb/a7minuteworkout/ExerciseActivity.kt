13fc1b11165a1584cc37d8759298c5be68eb420d
Rohit D Borkar <rohitborkar00@gmail.com>
2022-01-10 18:05:38 +0530

    Added progress bar

--- app/src/main/java/com/itsrdb/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/itsrdb/a7minuteworkout/ExerciseActivity.kt
@@ -2,9 +2,18 @@ package com.itsrdb.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0    //progress left
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -18,5 +27,42 @@ class ExerciseActivity : AppCompatActivity() {
         toolbarExercise.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setRestProgressBar(){
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val tvTimer = findViewById<TextView>(R.id.tvTimer)
+        progressBar.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) { //Every single tick
+                restProgress++
+                progressBar.progress = 10-restProgress
+                tvTimer.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "We will start the exercise now", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
     }
 }
