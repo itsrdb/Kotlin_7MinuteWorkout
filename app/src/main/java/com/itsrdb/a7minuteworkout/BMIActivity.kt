0d8a64b58c5a28bd300d03924f51a6491edd2ffa
Rohit D Borkar <rohitborkar00@gmail.com>
2022-01-23 04:01:25 +0530

    bmi activity ui

--- app/src/main/java/com/itsrdb/a7minuteworkout/BMIActivity.kt
+++ app/src/main/java/com/itsrdb/a7minuteworkout/BMIActivity.kt
@@ -2,12 +2,15 @@ package com.itsrdb.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_bmiactivity.*
+import android.view.View
+import kotlinx.android.synthetic.main.activity_bmi.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmiactivity)
+        setContentView(R.layout.activity_bmi)
 
         setSupportActionBar(toolbar_bmi_activity)
 
@@ -19,5 +22,78 @@ class BMIActivity : AppCompatActivity() {
         toolbar_bmi_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        btnCalculateUnits.setOnClickListener{
+            if(validateMetricUnits()){
+                val heightValue: Float = etMetricUnitHeight.text.toString().toFloat()/100
+                val weightValue: Float = etMetricUnitWeight.text.toString().toFloat()
+
+                val bmi = weightValue/(heightValue*heightValue)
+                displayBDMIResult(bmi)
+            }
+        }
+    }
+
+    private fun validateMetricUnits(): Boolean{
+        var isValid = true
+        if(etMetricUnitWeight.text.toString().isEmpty()){
+            isValid = false
+        }
+        if(etMetricUnitHeight.text.toString().isEmpty()){
+            isValid = false
+        }
+        return isValid
+    }
+
+    private fun displayBDMIResult(bmi: Float){
+        val bmiLabel: String
+        val bmiDescription: String
+
+        if (bmi.compareTo(15f) <= 0) {
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
+        ) {
+            bmiLabel = "Severely underweight"
+            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
+        ) {
+            bmiLabel = "Underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
+        ) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
+                bmi,
+                30f
+            ) <= 0
+        ) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
+        ) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0
+        ) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        tvYourBMI.visibility = View.VISIBLE
+        tvBMIValue.visibility = View.VISIBLE
+        tvBMIType.visibility = View.VISIBLE
+        tvBMIDescription.visibility = View.VISIBLE
+
+        // This is used to round the result value to 2 decimal values after "."
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        tvBMIValue.text = bmiValue // Value is set to TextView
+        tvBMIType.text = bmiLabel // Label is set to TextView
+        tvBMIDescription.text = bmiDescription // Description is set to TextView
     }
 }
