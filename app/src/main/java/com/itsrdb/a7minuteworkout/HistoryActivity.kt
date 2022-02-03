55deca7fecb64eb87b41f47f7f4fc833404baaa7
Rohit D Borkar <rohitborkar00@gmail.com>
2022-02-04 02:01:09 +0530

    implemented recycler view

--- app/src/main/java/com/itsrdb/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/itsrdb/a7minuteworkout/HistoryActivity.kt
@@ -3,6 +3,8 @@ package com.itsrdb.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_bmi.*
 import kotlinx.android.synthetic.main.activity_history.*
 
@@ -29,8 +31,18 @@ class HistoryActivity : AppCompatActivity() {
         val dbHandler = SqliteOpenHelper(this, null)
         val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
 
-        for(i in allCompletedDatesList){
-            Log.i("Date", ""+i)
+        if(allCompletedDatesList.size>0){
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+            rvHistory.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+            rvHistory.adapter = historyAdapter
+        }else{
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
         }
     }
 
